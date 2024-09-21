dd4862c7a6678a22f4bb1108063486a87d18458c
Tomoya Mikami <trymorecc@gmail.com>
2024-09-21 15:51:36 +0900

    accelerometerをhooksに

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,57 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Accelerometer, AccelerometerMeasurement } from 'expo-sensors';
-import { useEffect, useState } from 'react';
-import { Subscription } from 'expo-sensors/build/Pedometer';
-
-interface Accelerometer3Axis {
-  x: number
-  y: number
-  z: number
-}
-
-const round = (n: number | null) => {
-  if (!n) {
-    return 0;
-  }
-
-  return Math.floor(n * 100);
-};
+import { useAccelerometer } from './hooks/UseAccelerometer';
 
 export default function App() {
-  const [accelerometer3Axis, setAccelerometer3Axis] = useState<AccelerometerMeasurement>({
-    x: 0,
-    y: 0,
-    z: 0,
-    timestamp: 0,
-  });
-  const [
-    accelerometer3AxisSubscription,
-    setSubscriptionAccelerometer3AxisSubscription
-  ] = useState<Subscription|null>(null);
-
-  const _subscribe = () => {
-    Accelerometer.setUpdateInterval(100);
-    setSubscriptionAccelerometer3AxisSubscription(Accelerometer.addListener(setAccelerometer3Axis));
-  };
-
-  const _unsubscribe = () => {
-    accelerometer3AxisSubscription && accelerometer3AxisSubscription.remove();
-    setSubscriptionAccelerometer3AxisSubscription(null);
-  };
-
-  useEffect(() => {
-    _subscribe();
-    return () => _unsubscribe();
-  }, []);
+  const accelerometer3Axis = useAccelerometer();
 
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Text style={styles.text}>x: {round(accelerometer3Axis.x)}</Text>
-      <Text style={styles.text}>y: {round(accelerometer3Axis.y)}</Text>
-      <Text style={styles.text}>z: {round(accelerometer3Axis.z)}</Text>
+      <Text style={styles.text}>x: {accelerometer3Axis.x}</Text>
+      <Text style={styles.text}>y: {accelerometer3Axis.y}</Text>
+      <Text style={styles.text}>z: {accelerometer3Axis.z}</Text>
     </View>
   );
 }
---
+++ mobile-app/hooks/UseAccelerometer.tsx
@@ -0,0 +1,53 @@
+import { AccelerometerMeasurement, Accelerometer } from "expo-sensors";
+import { Subscription } from "expo-sensors/build/Pedometer";
+import { useState, useEffect } from "react";
+
+const round = (n: number | null) => {
+    if (!n) {
+      return 0;
+    }
+  
+    return Math.floor(n * 100);
+};
+
+export const useAccelerometer = (): AccelerometerMeasurement => {
+    const [accelerometer3Axis, setAccelerometer3Axis] = useState<AccelerometerMeasurement>({
+        x: 0,
+        y: 0,
+        z: 0,
+        timestamp: 0,
+    });
+
+    const accelerometerListener = (measurement: AccelerometerMeasurement) => {
+        setAccelerometer3Axis(
+            {
+                x: round(measurement.x),
+                y: round(measurement.y),
+                z: round(measurement.z),
+                timestamp: measurement.timestamp
+            }
+        )
+    }
+
+    const [
+        subscription,
+        setSubscription
+    ] = useState<Subscription | null>(null);
+
+    const _subscribe = () => {
+        Accelerometer.setUpdateInterval(100);
+        setSubscription(Accelerometer.addListener(accelerometerListener));
+    };
+
+    const _unsubscribe = () => {
+        subscription && subscription.remove();
+        setSubscription(null);
+    };
+
+    useEffect(() => {
+        _subscribe();
+        return () => _unsubscribe();
+    }, []);
+
+    return accelerometer3Axis;
+}
